e426d78a14e2429cbd852d03964577f8fc03e7d8
NikitaSokolov96 <107963433+NikitaSokolov96@users.noreply.github.com>
2022-07-18 02:00:14 +0300

    Add files via upload

---
+++ seminar_5/Task_38/Task_38.cs
@@ -0,0 +1,53 @@
+﻿static void DifOfMinAndMax(string[] args)
+
+{
+double dif = 0;
+
+double sum = 0;
+
+int max;
+
+int min;
+
+Console.WriteLine("Enter how many numbers u want");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[size];
+
+max = arr[0];
+
+min = arr[0];
+
+for (int i = 0; i < size; i++)
+
+{
+
+arr[i] = (new Random()).Next(1,100);
+
+if(arr[i] > max)
+
+{
+
+max=arr[i];
+
+}
+
+if (arr[i] < max)
+
+{
+
+    min = arr[i];
+
+}
+sum += arr[i];
+
+Console.WriteLine(arr[i] + " ");
+}
+
+sum = sum - max - min;
+dif = max - min;
+Console.WriteLine("sum = {0}, difference = {1}, max = {2}, min = {3}", sum, dif, max, min);
+}
+
+DifOfMinAndMax(args);
